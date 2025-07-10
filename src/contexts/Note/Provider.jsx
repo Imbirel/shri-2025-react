dcaa6888b3eac350ac02de61fa616a9c6db8fae5
Imbirel <katelin.kzm@gmail.com>
2025-07-10 05:44:12 +0300

    refactor: transferring work with local storage and form verification to a separate file

--- src/contexts/Note/Provider.jsx
+++ src/contexts/Note/Provider.jsx
@@ -1,16 +1,12 @@
 import { useEffect, useState } from 'react';
 import { NotesContext } from './context';
+import { NoteService } from '../../services/note';
 
-const STORAGE_KEY = 'notes';
-
-export const NoteProvider = ({ children, storageKey = STORAGE_KEY }) => {
-  const [notes, setNotes] = useState(() => {
-    const savedNotes = JSON.parse(localStorage.getItem(storageKey));
-    return savedNotes ? savedNotes : [];
-  });
+export const NoteProvider = ({ children }) => {
+  const [notes, setNotes] = useState(() => NoteService.getNotes());
 
   useEffect(() => {
-    localStorage.setItem(storageKey, JSON.stringify(notes));
+    NoteService.setNotes(notes);
   }, [notes]);
 
   return <NotesContext.Provider value={{ notes, setNotes }}>{children}</NotesContext.Provider>;
---
+++ src/services/note.js
@@ -0,0 +1,21 @@
+const STORAGE_KEY = 'notes';
+
+export const NoteService = {
+  setNotes(notes) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(notes));
+  },
+
+  getNotes() {
+    const savedNotes = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    return savedNotes ? savedNotes : [];
+  },
+
+  verify(note) {
+    if (!note.title.trim()) {
+      return false;
+    }
+
+    return true;
+  },
+};
